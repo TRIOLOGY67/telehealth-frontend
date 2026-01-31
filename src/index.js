5793a8a0f817853924e565806ddb716f8918f796
TRIOLOGY67 <basirajujaswanth@gmail.com>
2026-01-31 15:45:13 +0530

    Fix routing using HashRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
-
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
