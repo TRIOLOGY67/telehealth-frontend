19015268247727589376db6cb4fc4df4874d0713
TRIOLOGY67 <basirajujaswanth@gmail.com>
2026-01-31 15:52:25 +0530

    Fix double router issue (GitHub Pages)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Landing from "./pages/Landing";
 import PatientSignup from "./pages/PatientSignup";
 import DoctorSignup from "./pages/DoctorSignup";
@@ -8,36 +9,26 @@ import DoctorDashboard from "./pages/DoctorDashboard";
 import MapPage from "./pages/Map";
 import InbuiltChatbot from "./pages/InbuiltChatbot";
 import SymptomsReport from "./pages/SymptomsReport";
-
 import SmartSymptomChecker from "./pages/SmartSymptomChecker";
-
 import BookAppointment from "./pages/BookAppointment";
 import DoctorAppointment from "./pages/DoctorAppointment";
 
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/patient-signup" element={<PatientSignup />} />
-        <Route path="/doctor-signup" element={<DoctorSignup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/patient-dashboard" element={<PatientDashboard />} />
-        <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
-        <Route path="/map" element={<MapPage />} />
-        <Route path="/chatbot" element={<InbuiltChatbot />} />
-        <Route path="/symptoms-report" element={<SymptomsReport />} />
-        <Route path="/smart-symptom-checker" element={<SmartSymptomChecker />} />
-        <Route path="/book-appointment" element={<BookAppointment />} />
-<Route path="/doctor-appointments" element={<DoctorAppointment />} />
-
-
-
-
-
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/patient-signup" element={<PatientSignup />} />
+      <Route path="/doctor-signup" element={<DoctorSignup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/patient-dashboard" element={<PatientDashboard />} />
+      <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+      <Route path="/map" element={<MapPage />} />
+      <Route path="/chatbot" element={<InbuiltChatbot />} />
+      <Route path="/symptoms-report" element={<SymptomsReport />} />
+      <Route path="/smart-symptom-checker" element={<SmartSymptomChecker />} />
+      <Route path="/book-appointment" element={<BookAppointment />} />
+      <Route path="/doctor-appointments" element={<DoctorAppointment />} />
+    </Routes>
   );
 }
 
